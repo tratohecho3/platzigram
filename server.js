fcbc4e84867ffced3e0a6846c4f2d7fafcac5b8a
Cesar <tratohecho3@gmail.com>
2018-04-07 17:57:09 -0400

    static files and materialize library implemented

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/',(req,res) => res.send('Hola Mundo'));
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.get('/',(req,res) =>{
+    res.render('index');
+});
 app.listen(3000,(err) => {
     if(err){console.log(err)}
     console.log('La app esta corriendo en el puerto 3000');
