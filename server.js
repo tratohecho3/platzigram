56371803d93dee914c9b79bc89636b1d64b0fc73
Cesar <tratohecho3@gmail.com>
2018-05-02 15:45:22 -0400

    finished

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.get('/api/pictures', function (req, res, next) {
     {
       user: {
         username: 'slifszyc',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+        avatar: 'office.jpg'
       },
       url: 'office.jpg',
       likes: 0,
@@ -46,7 +46,7 @@ app.get('/api/pictures', function (req, res, next) {
     {
       user: {
         username: 'slifszyc',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+        avatar: 'office.jpg'
       },
       url: 'office.jpg',
       likes: 1,
