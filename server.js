e677091a6b81ed580c6a7f5125273c2eb58da20f
Cesar <tratohecho3@gmail.com>
2018-05-01 23:08:42 -0400

    translator and mock server implemented

--- server.js
+++ server.js
@@ -18,6 +18,34 @@ app.get('/signin', function (req, res) {
   res.render('index', { title: 'Platzigram - Signin' });
 })
 
+app.get('/api/pictures', function (req,res){
+
+  var pictures = [
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'office.jpg'
+      },
+      url: 'office.jpg',
+      likes: 0,
+      liked: false,
+      createdAt: new Date().getTime()
+    },
+    {
+      user: {
+        username: 'slifszyc',
+        avatar: 'office.jpg'
+      },
+      url: 'office.jpg',
+      likes: 12,
+      liked: true,
+      createdAt: new Date().setDate(new Date().getDate() - 10)
+    }
+  ];
+  setTimeout(function(){res.send(pictures);},2000)
+  
+})
+
 app.listen(3000, function (err) {
   if (err) return console.log('Hubo un error'), process.exit(1);
 
--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -2,5 +2,21 @@ module.exports = {
   likes: '{ likes, plural, ' +
           '=0 { no likes }' +
           '=1 { # like }' +
-          'other { # likes }}'
+          'other { # likes }}',
+  'logout': 'Logout',
+  'english': 'English',
+  'spanish': 'Spanish',
+  'signup.subheading': 'Signup to watch your friends\' students',
+  'signup.facebook': 'Signup with Facebook',
+  'signup.text': 'Signup',
+  'email': 'Email',
+  'username': 'User name',
+  'fullname': 'Full name',
+  'password': 'Password',
+  'signup.call-to-action': 'Singup',
+  'signup.have-account': 'Already have an account?',
+  'signin': 'Signin',
+  'signin.not-have-account': 'Don\'t have an account?',
+  'language': 'Language'
+  
 }
